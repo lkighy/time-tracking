d919459b12d258cb456e37a47bdd634ebd4d413d
lkighy <1003027913@qq.com>
2019-11-06 13:05:21 +0800

    修改了 calendar 组件

--- src/component/time-tracker/calendar.jsx
+++ src/component/time-tracker/calendar.jsx
@@ -7,19 +7,19 @@ export default class Calendar extends React.Component {
         this.handleDays = this.handleDays.bind(this);
         this.headleLastMonthSwitch = this.headleLastMonthSwitch.bind(this);
         this.headleNextMonthSwitch = this.headleNextMonthSwitch.bind(this);
+        this.handleSetDate = this.handleSetDate.bind(this);
 
         this.state = {
             monthArr: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-            toYear: 0,
-            toMonth: 0,
-            today: 0,
-            year: 0,
-            month: 0,
-            date: 0,
+            toYear: 0, // 选中的年份
+            toMonth: 0, // 选中的月份
+            today: 0, // 表示选中哪一天
+            year: 0, // 表示日历显示的当前年份
+            month: 0, // 表示日历显示的当前月份
             inDays: {
                 days: [],
-                daysNum: 0,
-                fromDay: 0,
+                daysNum: 0, // 这个月有多少天
+                fromDay: 0, // 从序列几开始
             },
         }
     }
@@ -27,12 +27,11 @@ export default class Calendar extends React.Component {
     componentDidMount() { // 挂载
         let date = new Date();
         this.setState({
-            toYear: date.getFullYear(),
-            toMonth: date.getMonth(),
-            today: date.getDate(),
-            year: date.getFullYear(),
-            month: date.getMonth(),
-            date: date.getDate(),
+            toYear: this.props.year || date.getFullYear(),
+            toMonth: this.props.month || date.getMonth(),
+            today: this.props.today || date.getDate(),
+            year: this.props.year || date.getFullYear(),
+            month: this.props.month || date.getMonth(),
         })
         this.handleDays(date.getFullYear(), date.getMonth());
     }
@@ -75,21 +74,30 @@ export default class Calendar extends React.Component {
     headleLastMonthSwitch() { // 换到上个月
         let month = this.state.month > 0 ? this.state.month - 1 : 11;
         let year = this.state.month > 0 ? this.state.year : this.state.year - 1;
-        this.setState({
+        this.setState(() => ({
             month,
             year
-        })
+        }))
         this.handleDays(year, month);
     }
 
     headleNextMonthSwitch() { // 换到下个月
         let month = this.state.month < 11 ? this.state.month + 1 : 0;
         let year = this.state.month < 11 ? this.state.year : this.state.year + 1;
-        this.setState({
+        this.setState(() => ({
             month,
             year
-        })
+        }))
         this.handleDays(year, month);
+    }
+
+    handleSetDate(year, month, day) {
+        // 设置日期
+        this.setState(() => ({
+            toYear: year - 0,
+            toMonth: month - 0,
+            today: day - 0
+        }))
 
     }
 
@@ -113,13 +121,14 @@ export default class Calendar extends React.Component {
                     <div>六</div>
                 </div>
                 <DaysList
-                    toYear={this.state.toYear}
+                    isStay={true}
                     toMonth={this.state.toMonth}
+                    toYear={this.state.toYear}
                     today={this.state.today}
                     year={this.state.year}
                     month={this.state.month}
-                    date={this.state.date}
                     inDays={this.state.inDays}
+                    handleSetDate={this.handleSetDate}
                 />
             </div>
         )
@@ -148,52 +157,90 @@ class YearSwitch extends React.Component {
 class DaysList extends React.Component {
     constructor(props) {
         super(props);
+
+        this.handleSwitchDay = this.handleSwitchDay.bind(this);
+    }
+
+    handleSwitchDay(e) {
+        // console.log(e.target.dataset.date)
+        // // date = e.target.dataset.date
+        // // if (this.props.handleSetDate) {
+        // //     this.props.handleSetDate(date)
+        // // }
+        let year = e.target.dataset.year;
+        let month = e.target.dataset.month;
+        let day = e.target.dataset.day;
+        this.props.handleSetDate(year, month, day)
     }
 
     render() {
         let month = this.props.month;
         let year = this.props.year;
+        let toMonth = this.props.toMonth;
+        let toYear = this.props.toYear;
+        let today = this.props.today;
+        let inDays = this.props.inDays;
 
-        let thisMonth = year + "," + month;
-        let lastMonth = month > 0 ? year + "," + (month - 1) : (year - 1) + "," + 11;
-        let nextMonth = month < 11 ? year + "," + (month + 1) : (year + 1) + "," + 1;
+        // let thisMonth = year + "/" + month;
+        // let lastMonth = month > 0 ? year + "/" + (month - 1) : (year - 1) + "/" + 11;
+        // let nextMonth = month < 11 ? year + "/" + (month + 1) : (year + 1) + "/" + 0;
 
         let days = [];
-        for (let i = 0; i < parseInt(this.props.inDays.days.length / 7); i++) {
+
+        for (let i = 0; i < parseInt(inDays.days.length / 7); i++) {
             let pack = [];
             let isStay = false;
-            let data;
+            // let data;
             for (let j = i * 7; j < i * 7 + 7; j++) {
-                let day = this.props.inDays.days[j];
-                let today = this.props.toYear + "," + this.props.toMonth + "," + this.props.today;
-                let key;
-                let style;
+                let day = inDays.days[j];
 
-                if (j < this.props.inDays.fromDay) {
-                    key = lastMonth + "," + day
-                    isStay = isStay || (key == today)
-                    style = "no-this "+ (key == today ? "current" : "");
-                } else if (j > this.props.inDays.fromDay + this.props.inDays.daysNum - 1) {
-                    key = nextMonth + "," + day
-                    isStay = isStay || (key == today)
-                    style = "no-this "+ (key == today ? "current" : "");
-                } else {
-                    key = thisMonth + "," + day
-                    isStay = isStay || (key == today)
-                    style = key == today ? "current" : "";
+                let inMonth = month;
+                let inYear = year;
+
+                if (j < inDays.fromDay) {
+                    if (inMonth > 0) {
+                        inMonth--;
+                    } else {
+                        inMonth = 11;
+                        inYear--;
+                    }
+                    // month > 0 ? month-- : (month + 11) & year--; // 一行搞定,但是会存在问题, 所以不采用
+
+                } else if (j > inDays.fromDay + inDays.daysNum - 1) {
+                    if (inMonth < 11) {
+                        inMonth++;
+                    } else {
+                        inMonth = 0;
+                        inYear++;
+                    }
+                    // month < 11 ? month++:(month-=11)&year++; // 存在问题,不采用
                 }
 
-                data = <div 
-                key={key}
-                date-date={key}
-                className={style}
-                >{day}</div>
+                let className = "";
+                if (toYear == inYear && toMonth == inMonth && day == today) {
+                    className += "current ";
+                }
 
-                pack.push(data);
+                if (inYear != year || inMonth != month) {
+                    className += "no-this"
+                }
+                pack.push(
+                    <div
+                        key={j}
+                        data-month={inMonth}
+                        data-year={inYear}
+                        data-day={day}
+                        className={className}
+                        onClick={this.handleSwitchDay}
+                    >
+                        {day}
+                    </div>
+                )
             }
-            let stay = (isStay ? "stay" : "");
+            let stay = this.props.isStay && isStay ? "stay" : "";
             days.push(<div className={"pack " + stay} key={i} >{pack}</div>);
         }
+
         return (
             <div className="days">
                 {days}
@@ -201,7 +248,6 @@ class DaysList extends React.Component {
         )
     }
 }
-
 
 // 计算一个月多少天
 function getMonthToDay(year, month) {
